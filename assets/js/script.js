b9d891ff2d2636398bf015bd7f88c0377c488779
Primo18 <danilobo.elo@gmail.com>
2024-03-08 01:47:12 -0300

    Add custom styles for popover and initialize Bootstrap Tooltip and Popover

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+// Initialize Bootstrap Tooltip and Popover
+
+// Tooltip
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// Popover
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+});
